c1c73b07fdf385c1393f32dc5f1b5b8d3bd0e18c
Bhavya Shah <bnshah@amazon.com>
2024-10-09 20:35:42 +0000

    generalize training setup

--- examples/train.sh
+++ examples/train.sh
@@ -2,14 +2,24 @@
 
 source ./train_setup.sh
 
-# For SFT: use the config file megatron_llama2_7B_SFT_config or megatron_llama3_8B_SFT_config
-: ${CONF_FILE:=megatron_gpt_config} 
+: ${CONF_FILE:=megatron_gpt_config}
+
+CONF_FILE_PATH="./conf/${CONF_FILE}.yaml"
+
+if [ -f "$CONF_FILE_PATH" ]; then
+    devices=$(grep 'devices:' "$CONF_FILE_PATH" | awk '{print $2}')
+    export PROCESSES_PER_NODE=$devices
+else
+    echo "Error: YAML file '$CONF_FILE_PATH' not found!"
+fi
+
+export DISTRIBUTED_ARGS="--nproc_per_node $PROCESSES_PER_NODE --nnodes $NTASKS --node_rank $NODEID --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+echo $DISTRIBUTED_ARGS
 
 if [ "$COMPILE" = "1" ]; then
     echo "compiling only run"
     MAYBE_COMPILE="neuron_parallel_compile"
 fi
-
 
 $MAYBE_COMPILE torchrun $DISTRIBUTED_ARGS training_orchestrator.py  \
     --config-path=conf \
