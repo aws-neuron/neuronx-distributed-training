b43d87dcc2c65557759aac71c26577e9ac29bc83
Kavish Gandhi <kvshbg@amazon.com>
2024-11-12 11:15:18 -0800

    fix: recompute modules logic; seq_move_factor bug fix
    cr: https://code.amazon.com/reviews/CR-159753250

--- src/neuronx_distributed_training/lightning_modules/nlp_overrides.py
+++ src/neuronx_distributed_training/lightning_modules/nlp_overrides.py
@@ -660,11 +660,13 @@ class NLPTrainer(Trainer):
         multiple_trainloader_mode: str = "max_size_cycle",
         inference_mode: bool = True,
         lnc: int = None,
+        sequential_move_factor: Optional[int] = None,
     ) -> None:
         Trainer._log_api_event("init")
         logging.info(f"{self.__class__.__name__}: Initializing trainer with parameters: {locals()}")
         self.state = TrainerState()
         self.lnc = get_lnc_size(lnc)
+        self.sequential_move_factor = sequential_move_factor
 
         if default_root_dir is not None:
             default_root_dir = os.fspath(default_root_dir)
